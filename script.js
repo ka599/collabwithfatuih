e944a17893705c5cb7674b0b7790879fcd52dddc
arya <aryachakabrajawisesa@gmail.com>
2025-11-16 10:58:39 +0700

    new fitur

--- script.js
+++ script.js
@@ -43,3 +43,42 @@ window.addEventListener("scroll", () => {
 
 // pagination
 
+document.addEventListener("DOMContentLoaded", () => {
+  const elementsToAnimate = document.querySelectorAll('.home-kopium');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible'); 
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, {
+    threshold: 0.1 
+  });
+
+  elementsToAnimate.forEach(el => {
+    observer.observe(el);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elementsToAnimate = document.querySelectorAll('.story');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible'); 
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, {
+    threshold: 0.1 
+  });
+
+  elementsToAnimate.forEach(el => {
+    observer.observe(el);
+  });
+});
